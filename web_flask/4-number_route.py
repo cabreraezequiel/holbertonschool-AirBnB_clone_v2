3889ba3c1cf2af90a94bbdc750b2a6ca800ee753
Ezequiel Cabrera <ezequielcabrera2601@gmail.com>
2022-09-05 11:14:21 -0700

    Fixed task 4

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -28,11 +28,9 @@ def python_text(text='is cool'):
     return ("Python {}".format(text.replace('_', ' ')))
 
 
-@app.route('/number/<n>', strict_slashes=False)
+@app.route('/number/<int:n>', strict_slashes=False)
 def number_n(n):
-    if (n.isdigit()):
-        return ("{} is a number".format(n))
-    return
+    return ("{} is a number".format(n))
 
 
 if __name__ == '__main__':
